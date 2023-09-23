85c0e14b67d78239f3d81b46a8e59317b62af46c
Hansyuf <108716867+Hansyuf@users.noreply.github.com>
2023-09-23 22:42:49 +0800

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,11 @@
+class Employee
+{
+    public string Name { get; set; }
+    public string Task { get; set; }
+
+    public Employee(string name, string task)
+    {
+        Name = name;
+        Task = task;
+    }
+}
